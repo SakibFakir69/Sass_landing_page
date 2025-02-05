c9718cb8a0a7322b280adfae7ef6cbd2fd8e8f09
Sakib Fakir <fakirsakib22232>
2025-02-06 01:31:19 +0600

    project start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1 className='text-xl'>hello</h1>
+      <h3 className='text-red-400 '>df</h3>
+
+      
+    </div>
+  )
+}
+
+export default App
